d1d8dfd3ca8918ca2c025954b914ff9d4eac4912
Matthieu <contact@matthieudurand.fr>
2017-04-18 15:29:42 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("node CMS ok !!!!!!");
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter as router, Route, Switch} from 'react-router-dom';
+import "./styles/bases.scss";
+
+ReactDOM.render(
+	<h1> Node Kickstarter </h1>,
+	document.getElementById("grid_app")
+	);
+
+console.log("hello from src/index.js !!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,96 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var path = require('path');
 
+var webpack = require('webpack');
+
+// process.env.NODE_ENV variable globale de node
+var isProduction = process.env.NODE_ENV === "production";
+
+// les fichier vont etre lu de droite vers la gauche
+var cssDev = ["style-loader", "css-loader", "sass-loader"];
+
+var cssProd = ExtractTextPlugin.extract({
+	fallback: "style-loader",
+	use: [
+	"css-loader","sass-loader"
+	],
+	publicPath:"./dist"
+});
+
+var cssConfig = (isProduction) ? cssProd : cssDev;
+
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: {
+		app : "./src/index.js",
+	},
+	target: "web",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	resolve:{
+		extensions: [".js", ".json"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: cssConfig
+			},
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: "babel-loader"
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: [/node_modules/],
+				loader: ["react-hot-loader", "babel-loader"]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|ico)$/i,
+				use: ["file-loader?name=[name].[ext]&outputPath=/assets/img/", "image-webpack-loader?bypassOnDebug"]
+			},
+			{
+				test: /\.json$/,
+				loader: "json"
+			},
+
+		]
+	},
+	devServer: {
+		compress: true,
+		contentBase: path.join(__dirname, "dist"),
+		hot: true,
+		open: true,
+		port: 3000,
+		stats: "errors-only"
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			allChunks: true,
+			disable: !isProduction,
+			filename: "style.css"
+		}),
+		new HTMLWebpackPlugin({
+			hash: true,
+			inject: "body",
+			minify: {
+				collapseWhitespace: false
+			},
+			template: path.resolve(__dirname, "src/templates/index.ejs"),
+			title: "node-kickstarter"
+		}),
+
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin()
+	]
 };
+
+
+
+
+
